4bc7dace538650a478104b2b66ddfe45f3caf7da
vasilikipsk <168685373+vasilikipsk@users.noreply.github.com>
2026-01-03 01:07:04 +0200

    Create extra.sql

---
+++ database/Vas/Project2/extra.sql
@@ -0,0 +1,3 @@
+  για να μη έχουμε error στο Foreign Key
+INSERT INTO sources (source_id, name, type_) VALUES (1, 'Coursera', 'csv');
+INSERT INTO sources (source_id, name, type_) VALUES (2, 'Udacity', 'json');
