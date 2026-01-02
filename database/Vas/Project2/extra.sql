de2cc7ad44777605f92ae0451e1781f33aecab10
vasilikipsk <168685373+vasilikipsk@users.noreply.github.com>
2026-01-03 01:08:14 +0200

    Update extra.sql

--- database/Vas/Project2/extra.sql
+++ database/Vas/Project2/extra.sql
@@ -1,3 +1,4 @@
-  για να μη έχουμε error στο Foreign Key
+-- για να μη έχουμε error στο Foreign Key
+-- το βάζουμε στο phpMyAdmin αφου βάλουμε την βάση 
 INSERT INTO sources (source_id, name, type_) VALUES (1, 'Coursera', 'csv');
 INSERT INTO sources (source_id, name, type_) VALUES (2, 'Udacity', 'json');
