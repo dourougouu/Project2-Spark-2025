b3fedb4817bc090cc1f7ca97ef4acfb83828172d
vasilikipsk <168685373+vasilikipsk@users.noreply.github.com>
2026-01-28 14:39:07 +0200

    Add files via upload

---
+++ FINAL/ml_spark/spark_ml_service.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, HashingTF, IDF
+from pyspark.ml.clustering import KMeans
+from pyspark.ml import Pipeline
+import numpy as np
+from pyspark.sql.functions import col
+
+# 1. Δημιουργία Spark Session
+spark = SparkSession.builder \
+    .appName("Course_ML_Pipeline") \
+    .getOrCreate()
+
+# 2. Φόρτωση του Ενοποιημένου Αποθετηρίου (Βήμα 4.2 & 4.3)
+df = spark.read.option("multiLine", "true").json("unified_repository.json")
+
+df = df.fillna({"summary": "", "title": "", "source_course_id": ""}) #αν summary=null να μην μας βγαλει error στο tokenizer
+
+df = df.withColumn("full_text", concat_ws(" ", df["title"], df["summary"]))
+
+# 3. Προετοιμασία Δεδομένων (Data Preprocessing - Βήμα 4.4)
+tokenizer = Tokenizer(inputCol="full_text", outputCol="words")
+remover = StopWordsRemover(inputCol="words", outputCol="filtered")
+
+# 4. Vectorization με TF-IDF (Βήμα 4.4.1)
+hashingTF = HashingTF(inputCol="filtered", outputCol="rawFeatures", numFeatures=1000)
+idf = IDF(inputCol="rawFeatures", outputCol="features")
+
+# 5. Clustering Μαθημάτων με K-Means (Βήμα 4.4.2)
+kmeans = KMeans(k=5, seed=1).setFeaturesCol("features").setPredictionCol("cluster_id")
+
+# 6. Δημιουργία και Εκτέλεση Pipeline
+pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf, kmeans])
+model = pipeline.fit(df)
+results = model.transform(df)
+
+# 7. Export Αποτελεσμάτων για το API (Βήμα 4.4.3)
+# Επιλέγουμε τα απαραίτητα πεδία: Τίτλο, Cluster ID και τα TF-IDF Features (για Similarity)
+final_results = results.select("title", "source_course_id", "cluster_id", "features")
+
+# Αποθήκευση σε JSON μορφή που θα διαβάσει το API
+final_results.write.mode("overwrite").json("ml_results.json")
+
+print("Το Spark ML Pipeline ολοκληρώθηκε επιτυχώς!")
+spark.stop()
